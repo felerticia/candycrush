95f3f1a9ff2f6325c855c95b904a4355b606cb86
Amirhossein Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-01-06 16:31:30 +0100

    create board

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,27 @@ import orangeCandy from "./images/orange-candy.png";
 import purpleCandy from "./images/purple-candy.png";
 import redCandy from "./images/red-candy.png";
 import yellowCandy from "./images/yellow-candy.png";
+import { WIDTH, COLORS } from "./constants";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [candies, setCandies] = useState<string[]>([]);
+
+  const createBoard = () => {
+    const colors: string[] = new Array(WIDTH * WIDTH)
+      .fill(0)
+      .map(() => COLORS[Math.floor(Math.random() * COLORS.length)]);
+
+    return colors;
+  };
+
+  useEffect(() => {
+    const board = createBoard();
+    setCandies(board);
+  }, []);
+
+  console.log(candies);
+
   return (
     <div>
       <img src={blueCandy} />
