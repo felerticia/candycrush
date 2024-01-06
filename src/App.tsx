3da4f2de76a2fdadbc406c507eac6b3afba65bbc
Amirhossein Shahbabaee <amirhossein.shahbabaee@cleura.com>
2024-01-06 17:04:13 +0100

    Remove duplicates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import purpleCandy from "./images/purple-candy.png";
 import redCandy from "./images/red-candy.png";
 import yellowCandy from "./images/yellow-candy.png";
 import { WIDTH, COLORS } from "./constants";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 function App() {
   const [candies, setCandies] = useState<string[]>([]);
@@ -18,10 +18,141 @@ function App() {
     return colors;
   };
 
-  useEffect(() => {
-    const board = createBoard();
-    setCandies(board);
+  const checkColumn = (board: string[]) => {
+    for (let i = 0; i <= WIDTH * (WIDTH - 4) - 1; i++) {
+      if (!board[i]) continue;
+
+      if (
+        board[i] === board[i + WIDTH] &&
+        board[i] === board[i + WIDTH * 2] &&
+        board[i] === board[i + WIDTH * 3] &&
+        board[i] === board[i + WIDTH * 4]
+      ) {
+        board[i] = "";
+        board[i + WIDTH] = "";
+        board[i + WIDTH * 2] = "";
+        board[i + WIDTH * 3] = "";
+        board[i + WIDTH * 4] = "";
+      }
+    }
+
+    for (let i = 0; i <= WIDTH * (WIDTH - 3) - 1; i++) {
+      if (!board[i]) continue;
+
+      if (
+        board[i] === board[i + WIDTH] &&
+        board[i] === board[i + WIDTH * 2] &&
+        board[i] === board[i + WIDTH * 3]
+      ) {
+        board[i] = "";
+        board[i + WIDTH] = "";
+        board[i + WIDTH * 2] = "";
+        board[i + WIDTH * 3] = "";
+      }
+    }
+
+    for (let i = 0; i <= WIDTH * (WIDTH - 2) - 1; i++) {
+      if (!board[i]) continue;
+
+      if (board[i] === board[i + WIDTH] && board[i] === board[i + WIDTH * 2]) {
+        board[i] = "";
+        board[i + WIDTH] = "";
+        board[i + WIDTH * 2] = "";
+      }
+    }
+
+    return board;
+  };
+
+  const checkRow = (board: string[]) => {
+    for (let i = 0; i <= WIDTH * WIDTH - 5; i++) {
+      if (!board[i]) continue;
+
+      if ((i % WIDTH) + 5 > WIDTH) continue;
+
+      if (
+        board[i] === board[i + 1] &&
+        board[i] === board[i + 2] &&
+        board[i] === board[i + 3] &&
+        board[i] === board[i + 4]
+      ) {
+        board[i] = "";
+        board[i + 1] = "";
+        board[i + 2] = "";
+        board[i + 3] = "";
+        board[i + 4] = "";
+      }
+    }
+
+    for (let i = 0; i <= WIDTH * WIDTH - 4; i++) {
+      if (!board[i]) continue;
+
+      if ((i % WIDTH) + 4 > WIDTH) continue;
+
+      if (
+        board[i] === board[i + 1] &&
+        board[i] === board[i + 2] &&
+        board[i] === board[i + 3]
+      ) {
+        board[i] = "";
+        board[i + 1] = "";
+        board[i + 2] = "";
+        board[i + 3] = "";
+      }
+    }
+
+    for (let i = 0; i <= WIDTH * WIDTH - 3; i++) {
+      if (!board[i]) continue;
+
+      if ((i % WIDTH) + 3 > WIDTH) continue;
+
+      if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        board[i] = "";
+        board[i + 1] = "";
+        board[i + 2] = "";
+      }
+    }
+
+    return board;
+  };
+
+  const moveDown = (board: string[]) => {
+    for (let i = 0; i <= WIDTH * WIDTH; i++) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+      const isFirstRow = firstRow.includes(i);
+
+      if (isFirstRow && board[i] === "") {
+        const randomNumber = Math.floor(Math.random() * COLORS.length);
+        board[i] = COLORS[randomNumber];
+      }
+
+      if (board[i + WIDTH] === "") {
+        board[i + WIDTH] = board[i];
+        board[i] = "";
+      }
+    }
+    return board;
+  };
+
+  const clearDuplicates = useCallback((board: string[]) => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      let newBoard = [...board];
+      newBoard = checkColumn(newBoard);
+      newBoard = checkRow(newBoard);
+      newBoard = moveDown(newBoard);
+      if (board.every((cell, i) => cell === newBoard[i])) {
+        return board;
+      }
+      board = [...newBoard];
+    }
   }, []);
+
+  useEffect(() => {
+    let board = createBoard();
+    board = clearDuplicates(board);
+    setCandies(board);
+  }, [clearDuplicates]);
 
   console.log(candies);
 
